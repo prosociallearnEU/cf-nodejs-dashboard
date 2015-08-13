ecacd0e0fd6303abd6c252bfd65081989fef747c
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-08-13 15:27:09 +0200

    Update in the project. Running on local & pivotal

--- index.js
+++ index.js
@@ -14,17 +14,12 @@ var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //CF
-//var config = require('./config.json');
-//var API_URL = "http://api." + process.env.VCAP_APP_HOST + ".xip.io/v2/info";
-//var API_URL = "http://api." + process.env.VCAP_APP_HOST + ".xip.io/v2/info";
-//var API_URL = "http://api." + process.env.VCAP_APP_HOST + "/v2/info";
-//var API_URL = "http://api." + process.env.CF_INSTANCE_IP + "/v2/info";
-//var API_URL = "http://api." + process.env.CF_INSTANCE_ADDR + ".xip.io/v2/info";
-//var API_URL = "http://api." + process.env.CF_INSTANCE_IP + ".xip.io/v2/info";
-var API_URL = "https//api." + process.env.VCAP_APPLICATION.host; //":" + process.env.VCAP_APPLICATION.port
+var config = require('./config.json');
 var cloudFoundry = require("cf-nodejs-client").CloudFoundry;
-cloudFoundry = new cloudFoundry(API_URL);
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+var cloudFoundryApps = require("cf-nodejs-client").Apps;
+cloudFoundry = new cloudFoundry(config.CF_API_URL);
+cloudFoundryApps = new cloudFoundryApps(config.CF_API_URL);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 //WEB
 
@@ -35,8 +30,8 @@ app.get('/', function (req, res) {
 //REST API
 
 // POST /api/auth/login
-// @desc: logs in a user
-app.post("/api/auth/login", urlencodedParser, function (req, res) {
+// @desc: Login
+app.post("/api/auth/login", jsonParser, function (req, res) {
 
     var username = req.body.username;
     var password = req.body.password;
@@ -46,14 +41,36 @@ app.post("/api/auth/login", urlencodedParser, function (req, res) {
 
 	var token_endpoint = null;
 
-
 	cloudFoundry.getInfo().then(function (result) {
 		token_endpoint = result.token_endpoint;
-		//return token_endpoint;
-	    //return cloudFoundry.login(token_endpoint,config.username,config.password);
-	//}).then(function (result) {
-		res.json(result) 
-	    //res.json({ "auth_token" : result.token_type + " " + result.access_token});  
+	    return cloudFoundry.login(token_endpoint,config.username,config.password);
+	}).then(function (result) {
+	    res.json({ "auth_token" : result.token_type + " " + result.access_token});  
+	}).catch(function (reason) {
+	    res.json({"error": reason});
+	});
+
+});
+
+// POST /api/apps
+// @desc: get Apps
+app.get("/api/apps", jsonParser, function (req, res) {
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    console.log(username);
+    console.log(password);
+
+	var token_endpoint = null;
+
+	cloudFoundry.getInfo().then(function (result) {
+		token_endpoint = result.token_endpoint;	
+		return cloudFoundry.login(token_endpoint,config.username,config.password);
+	}).then(function (result) {
+            return cloudFoundryApps.getApps(result.token_type,result.access_token);
+	}).then(function (result) {
+        res.json(result); 
 	}).catch(function (reason) {
 	    res.json({"error": reason});
 	});
---
+++ public_html/Scripts/main.js
@@ -0,0 +1,94 @@
+
+var username = null;
+var password = null;
+
+$( document ).ready(function() {
+
+	function initApp(){
+		$("#pageLogin").show();
+		$("#pageHome").hide();
+		$("#pageApps").hide();
+		$("#pageDeployApps").hide();
+	}
+
+	initApp();
+
+	$("#pageLogin").find("#btnLoginSubmit").click(function(event) {
+		event.preventDefault();
+
+		var url = "/api/auth/login";
+		username = $("#username").val();
+		password = $("#password").val();
+
+		//Form validation
+		if ((!username.trim()) && (!password.trim())) {
+    		console.log("Form validation problem");
+    		return false;
+		}
+
+		var data = {
+			username: username, 
+			password: password
+		}
+
+		$.ajax({
+		    url: url, 
+		    type: 'POST', 
+		    contentType: 'application/json', 
+		    data: JSON.stringify(data)}
+		).done(function( data ) {
+		  	console.log(data);
+		  	$("#pageLogin").hide();
+		  	$("#pageHome").show();
+		});
+	});
+
+	$("#main").on("click","#btnLogout", function(event) {
+		event.preventDefault();
+
+		$("#pageLogin").show();
+		$("#pageHome").hide();
+		$("#pageApps").hide();
+		$("#pageDeployApps").hide();
+	});
+
+	$("#main").on("click","#btnHome", function(event) {
+		event.preventDefault();
+
+		$("#pageLogin").hide();
+		$("#pageHome").show();
+		$("#pageApps").hide();
+		$("#pageDeployApps").hide();
+	});
+
+	$("#main").on("click","#btnApps", function(event) {
+		event.preventDefault();
+
+		var url = "/api/apps";
+		$.ajax({
+		    url: url, 
+		    type: 'GET', 
+		    contentType: 'application/json'
+		}).done(function( data ) {
+		  	console.log(data);
+		  	//Process
+		  	$("#pageApps").find("#result").text(JSON.stringify(data));
+
+		  	$("#pageLogin").hide();
+			$("#pageHome").hide();
+			$("#pageApps").show();
+			$("#pageDeployApps").hide();			
+		});
+
+	});
+
+	$("#btnDeployApp").on("click", function(event) {
+		event.preventDefault();
+
+		$("#pageLogin").hide();
+		$("#pageHome").hide();
+		$("#pageApps").hide();
+		$("#pageDeployApps").show();		
+	});
+
+});
