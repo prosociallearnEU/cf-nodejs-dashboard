baf2bcef8a70349774f6967df1e8ed02d17bb39d
Antonio Diaz Arroyo <antonio.diaza@atos.net>
2016-03-11 12:30:01 +0100

    Update cf-nodejs library to version 13.0

--- services/SpaceService.js
+++ services/SpaceService.js
@@ -1,32 +1,30 @@
 /*jslint node: true*/
+"use strict";
 
-var CloudFoundry = require("cf-nodejs-client").CloudFoundry;
+var CloudController = require("cf-nodejs-client").CloudController;
 var CloudFoundryUsersUAA = require("cf-nodejs-client").UsersUAA;
 var CloudFoundrySpaces = require("cf-nodejs-client").Spaces;
-CloudFoundry = new CloudFoundry();
+
+CloudController = new CloudController();
 CloudFoundryUsersUAA = new CloudFoundryUsersUAA();
 CloudFoundrySpaces = new CloudFoundrySpaces();
 
 function SpaceServices(_CF_API_URL, _username, _password) {
-    "use strict";
     this.CF_API_URL = _CF_API_URL;
     this.username = _username;
     this.password = _password;
 }
 
 SpaceServices.prototype.setEndpoint = function (endpoint) {
-    "use strict";
     this.CF_API_URL = endpoint;
 };
 
 SpaceServices.prototype.setCredential = function (username, password) {
-    "use strict";
     this.username = username;
     this.password = password;
 };
 
 SpaceServices.prototype.getApps = function (space_guid) {
-    "use strict";
     var token_endpoint = null;
     var authorization_endpoint = null;
     var token_type = null;
@@ -37,13 +35,10 @@ SpaceServices.prototype.getApps = function (space_guid) {
     var apps = null;   
 
     return new Promise(function (resolve, reject) {
-
         try {
-
-            CloudFoundry.setEndPoint(self.CF_API_URL);
+            CloudController.setEndPoint(self.CF_API_URL);
             CloudFoundrySpaces.setEndPoint(self.CF_API_URL);
-
-            CloudFoundry.getInfo().then(function (result) {
+            CloudController.getInfo().then(function (result) {
                 token_endpoint = result.token_endpoint;
                 authorization_endpoint = result.authorization_endpoint;
                 CloudFoundryUsersUAA.setEndPoint(authorization_endpoint);
@@ -54,24 +49,18 @@ SpaceServices.prototype.getApps = function (space_guid) {
                 return CloudFoundrySpaces.getSpaceApps(token_type, access_token, space_guid);
             }).then(function (result) {
                 apps = result;
-
                 results = {
                     apps: apps
-                }                              
+                };                           
                 return resolve(results);
             }).catch(function (reason) {
                 console.error("Error: " + reason);
                 return reject(reason);
             });
-
         } catch (error) {
             return reject(error);
         }
-
     });
-
 };
-
-
 
 module.exports = SpaceServices;
